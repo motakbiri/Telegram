7f10e235066f8c8cc6caf1bff444fabef23501d9
Mohammad Takbiri <mo.takbiri@gmail.com>
2017-08-30 00:18:41 +0430

    add config files

---
+++ config.h
@@ -0,0 +1,14 @@
+//
+//  config.h
+//  Telegraph
+//
+//  Created by Mohammad Takbiri on 8/29/17.
+//
+//
+#ifndef config_h
+#define config_h
+
+#define SETUP_API_ID(v) v = <YOUR API ID>;
+#define SETUP_API_HASH(v) v = @"<YOUR API HASH>";
+
+#endif /* config_h */
